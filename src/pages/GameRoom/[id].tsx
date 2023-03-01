47f8207dd886f3fdb70b3e74c54357f75370b286
Dryspell <Nhwinslow@gmail.com>
2023-02-28 23:38:41 -0500

    Renamed VoteExchange to GameRoom

--- src/pages/GameRoom/[id].tsx
+++ src/pages/GameRoom/[id].tsx
@@ -3,12 +3,12 @@ import Head from "next/head";
 import React from "react";
 import GameRoom from "../../components/GameRoom/GameRoom";
 
-export default function VoteExchange() {
+export default function GameRoomPage() {
   return (
     <>
       <Head>
-        <title>VoteExchange</title>
-        <meta name="description" content="VoteExchange" />
+        <title>GameRoom</title>
+        <meta name="description" content="GameRoom" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <GameRoom />
